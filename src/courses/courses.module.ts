01e7060b048078207877f7b1dac86fa911aa8960
paladinze <paladinze@hotmail.com>
2020-02-21 16:08:19 +0800

    added custom middleware

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
+import { LoggerMiddleware } from 'middleware/logger.middleware';
 
 @Module({
   controllers: [CoursesController],
   providers: [CoursesService],
   exports: [CoursesService],
 })
-export class CoursesModule {}
+export class CoursesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('courses');
+  }
+}
